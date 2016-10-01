c559a4a3c2f1872a97b2a43a0e4a849d467ee838
Unknown <gabriel.lamarre@lepisc.com>
2016-09-30 22:37:02 -0400

    Amelioration #6

---
+++ Assets/Scripts/PlayerCoreController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCoreController : MonoBehaviour {
+
+    public GameObject player;
+    
+	void Update () {
+        Debug.Log(transform.position);
+        Debug.Log(player.transform.position);
+        transform.position = player.transform.position;
+	}
+}
